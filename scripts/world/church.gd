c45de363f1467164aa358d6203b7936408a2e1a4
dizenden <aidenlangba@gmail.com>
2024-11-17 00:36:02 +0530

    sfx

--- scripts/world/church.gd
+++ scripts/world/church.gd
@@ -3,5 +3,8 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	body_entered.connect(func(_body): $bells.play())
+	body_entered.connect(func(_body): 
+		$box.queue_free()
+		$bells.play()
+		)
 
