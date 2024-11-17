824dd096a9e1735cbd90817d3d4c20b486ec1399
dizenden <aidenlangba@gmail.com>
2024-11-17 21:09:59 +0530

    bg

--- scenes/levels/1.gd
+++ scenes/levels/1.gd
@@ -3,8 +3,11 @@ extends Node2D
 @export var bgm: AudioStream
 @export_group("Change only from parent scene")
 @export var mouse_mode: Input.MouseMode = Input.MOUSE_MODE_VISIBLE
+@export var bg_color: Color
 	
 func _ready() -> void:
+	if bg_color: 
+		RenderingServer.set_default_clear_color(bg_color)
 	Input.mouse_mode = mouse_mode
 	if not AudioManager.is_playing:
 		AudioManager.play(bgm)
